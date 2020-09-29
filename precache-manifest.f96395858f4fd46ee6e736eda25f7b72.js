0595b52dfe698117d0acb7301a05b9c15a44c037
pinklightsaber <ctorres@idemar.cl>
2020-09-29 13:10:57 -0300

    Updates

---
+++ precache-manifest.f96395858f4fd46ee6e736eda25f7b72.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "766e4f6794b258eb3d0f0d3a53af831e",
+    "url": "/portfolioreact/index.html"
+  },
+  {
+    "revision": "fd138a8a6e9be86b2cc3",
+    "url": "/portfolioreact/static/css/main.d7433353.chunk.css"
+  },
+  {
+    "revision": "c4341891120ffd0c0f44",
+    "url": "/portfolioreact/static/js/2.d3e171a3.chunk.js"
+  },
+  {
+    "revision": "df04310ad48f4f5411320a1942994090",
+    "url": "/portfolioreact/static/js/2.d3e171a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd138a8a6e9be86b2cc3",
+    "url": "/portfolioreact/static/js/main.9aca6b0a.chunk.js"
+  },
+  {
+    "revision": "ccc82b9744967cf75754",
+    "url": "/portfolioreact/static/js/runtime-main.45fc4603.js"
+  },
+  {
+    "revision": "476350925428b738a110bc2f046c50bb",
+    "url": "/portfolioreact/static/media/a1c27395998927.47635092.png"
+  },
+  {
+    "revision": "2b1ed77d299c751e62933dd1f5a7be0d",
+    "url": "/portfolioreact/static/media/du.2b1ed77d.png"
+  },
+  {
+    "revision": "daafbfe96b620e410926a7d3dc4602ae",
+    "url": "/portfolioreact/static/media/mine.daafbfe9.jpg"
+  },
+  {
+    "revision": "b10a1a8c6513316057633a1636d29599",
+    "url": "/portfolioreact/static/media/mine2.b10a1a8c.jpg"
+  },
+  {
+    "revision": "0fdd0c9af34d918ab494cda01d4ceb8c",
+    "url": "/portfolioreact/static/media/proyecto1.0fdd0c9a.JPG"
+  },
+  {
+    "revision": "981851b8d693a19062a3d82a31a83f2f",
+    "url": "/portfolioreact/static/media/proyecto2.981851b8.JPG"
+  },
+  {
+    "revision": "fb3c9dbddfa9406a3a8a76d2bd5f74fe",
+    "url": "/portfolioreact/static/media/proyecto3.fb3c9dbd.JPG"
+  }
+]);
